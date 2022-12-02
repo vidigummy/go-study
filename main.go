8bb1f8c1918f9a89033b072586d28564529ed13d
vidigummy <vidigummy@gmail.com>
2022-12-02 17:23:41 +0900

    쨥

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"example.com/m/v2/mydict"
-)
-
 func main() {
-	dictionary := mydict.Dictionary{}
-	baseword := "hello"
-	dictionary.Add(baseword, "greetings")
-	word, _ := dictionary.Search(baseword)
-	fmt.Println("삭제 전 : ", word)
-	dictionary.Delete(baseword)
-	deltedWord, err := dictionary.Search(baseword)
-	if err == nil {
-		fmt.Println("성공 : ", deltedWord)
-	} else {
-		fmt.Println(err)
-	}
-	delErr := dictionary.Delete(baseword)
-	if delErr != nil {
-		fmt.Println(delErr)
-	} else {
-		fmt.Println("왜 되냐")
-	}
+
 }
