f2bc2787ca8af23d934792df1ea8e98b896a7b78
vidigummy <vidigummy@gmail.com>
2022-12-02 22:46:01 +0900

    그냥 이케 하면 된다~

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"github.com/labstack/echo"
+)
+
 func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.File("home.html")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
